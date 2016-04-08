2f5375e94480cb55e29e24ca233bea9d75503219
Samuel Ruíz González <Samuel Ruz Gonzlez>
2016-04-08 18:18:11 +0100

    paso 1 y 2

---
+++ src/ut5examensamuelruizgonzalez/Calculadora.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ut5examensamuelruizgonzalez;
+
+/**
+ *
+ * @author samue
+ */
+public class Calculadora {
+        private int operador1;
+        private int operador2;
+        private String operacion;  
+}
